85e899b9011a5f3242ccb1d685e147b07be8827d
rajdeeppaul9999 <37372799+rajdeeppaul9999@users.noreply.github.com>
2018-03-22 23:42:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<html>
+<title>
+Index
+</title>
+<head>
+<style>
+	body
+	{
+		background-color:#FAEBD7;
+	}
+	
+	#mainpage
+	{
+		border:2px solid brown;
+		width:1000px;
+		height:100%;
+		margin:40px auto;
+	}
+	
+	#header
+	{
+		border:2px solid black;
+		width:99.6%;
+		background-color:#DEB887;
+		height:120px;
+		margin-top:0px;
+	}
+	
+	#menu
+	{
+		background-color:#87cc99;
+		
+	}
+	#menu ul
+	{
+		background-color:black;
+		margin:0px;
+		border:2px solid white;
+		border-radius:10px;
+		overflow:hidden;
+		height:50px;
+	
+	}	
+	#menu ul li
+	{
+		float:left;
+		border-right:2px solid white;
+		height:50px;
+		
+	}
+	#menu ul li a
+	{
+		font-size:35px;
+		color:white;
+		text-decoration: none;
+		margin:30px;55;
+		
+	}
+	#writing
+	{
+		
+		background-color:#EEE8AA;
+		width:99.8%;
+		position:relative;
+		margin:-20px auto;
+		padding-bottom:52.5%;
+		
+	}
+	#h2
+	{
+		paddind:20px;
+	}
+
+</style>
+</head>
+<body>
+	<div id="mainpage">
+	
+		<div id="header">
+		
+		<h1 align="center"> NAME </h1>
+		
+		</div>
+		<div id="menu">
+		
+			<ul>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">HOME</a></li>
+				<li><a href="3">HOME</a></li>
+			</ul>
+			
+		</div>
+		
+			<div id="writing"> 
+				
+				<h2 align="center">WRITING HERE WITH PARAGRAPH</h2>
+				
+				<p>TYPE HERE</P>
+				<p>TYPE HERE</P>
+				<p>TYPE HERE</P>
+				<p>TYPE HERE</P>
+				<p>TYPE HERE</P>
+				<p>TYPE HERE</P>
+				
+			</div>
+		
+	</div>
+
+</body>
+</html>
